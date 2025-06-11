ea4697f87906537e5da0574ad47a13c6deb6b7ac
Luk-ESC <eschbacher.lukas@gmail.com>
2025-06-11 11:14:00 +0200

    chore: init

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    naersk,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+      in rec {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+
+          postInstall = ''
+            #wrapProgram $out/bin/panoptes --SET WHAT bruh
+          '';
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [rustc cargo];
+        };
+
+        nixosModules = {
+          panoptes-service = {
+            config,
+            pkgs,
+            lib,
+            ...
+          }: {
+            options = {};
+            config = {
+              systemd.services.panoptes = {
+                description = "Panoptes Background Service";
+                after = ["network.target"];
+                wantedBy = ["multi-user.target"];
+
+                serviceConfig = {
+                  ExecStart = "${self.defaultPackage.${pkgs.system}}/bin/panoptes";
+                  Restart = "on-failure";
+                };
+              };
+            };
+          };
+        };
+      }
+    );
+}
---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use std::{borrow::Cow, io::ErrorKind, os::unix::fs::symlink, path::Path};
+
+use fanotify::{
+    high_level::{Fanotify, FanotifyMode},
+    low_level::FAN_CLOSE_WRITE,
+};
+
+fn build_tree(path: &Path, pid: i32) {
+    let base = Path::new("out/");
+
+    let mut path = base.join(path.strip_prefix("/").unwrap());
+    path.push("");
+
+    if let Err(e) = std::fs::create_dir_all(&path) {
+        if e.kind() != ErrorKind::AlreadyExists {
+            panic!("failed to build output tree for {path:?}: {e}");
+        }
+    }
+
+    let exe_path = format!("/proc/{pid}/exe");
+    match std::fs::read_link(&exe_path) {
+        Ok(target) => {
+            path.push(target.file_name().unwrap());
+
+            if let Err(e) = symlink(target, &path) {
+                if e.kind() != ErrorKind::AlreadyExists {
+                    panic!("failed to build symlink for {path:?}: {e}");
+                }
+            }
+        }
+        Err(e) => {
+            if e.kind() != ErrorKind::NotFound {
+                panic!("failed to build symlink for {exe_path:?}: {e}");
+            }
+        }
+    };
+}
+
+fn main() {
+    let fa = Fanotify::new_blocking(FanotifyMode::NOTIF).unwrap();
+    fa.add_mountpoint(FAN_CLOSE_WRITE, "/").unwrap();
+
+    let my_pid = std::process::id() as i32;
+    println!("Started listening! (my pid: {my_pid})");
+
+    loop {
+        let events = fa.read_event();
+        for i in events {
+            if i.pid == my_pid {
+                continue;
+            }
+
+            build_tree(Path::new(&i.path), i.pid);
+        }
+    }
+}
